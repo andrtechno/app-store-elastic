afa38d993a742ca3404fd14953802159a8284a44
Andrew <andrew.panix@gmail.com>
2023-05-25 01:11:38 +0300

    3

--- config/common.php
+++ config/common.php
@@ -56,6 +56,16 @@ $config = [
         'contacts' => ['class' => 'panix\mod\contacts\Module'],
     ],
     'components' => [
+        'elasticsearch' => [
+            'class' => 'yii\elasticsearch\Connection',
+            'auth' => ['username' => 'elastic', 'password' => ''],
+            'connectionTimeout' => 680,
+            'nodes' => [
+                ['http_address' => '127.0.0.1:9200'],
+                // configure more hosts if you have a cluster
+            ],
+            'dslVersion' => 7, // default is 5
+        ],
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
             'defaultRoles' => ['guest', 'user'],
